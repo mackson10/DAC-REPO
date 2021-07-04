3464d1194ab52b0ac6786ef58dd794e3d069cf87
MACKSON MATTOS <user.name>
2021-07-04 17:16:26 -0300

    tarefa 01 pronta para testes

--- src/main/java/hello/MessageBean.java
+++ src/main/java/hello/MessageBean.java
@@ -9,6 +9,9 @@ package hello;
 
 import java.beans.*;
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -17,29 +20,65 @@ import java.io.Serializable;
 public class MessageBean implements Serializable {
      
     private String msg;
-        
+    private String byeMsg;
+
+    private enum Greetings {
+        PT("Olá", "Bom dia", "Boa tarde", "Boa noite", "Tchau"),
+        EN("Hello", "Good morning", "Good afternoon", "Good night", "Goodbye"),
+        DE("Hallo", "Guten Morgen", "Guten Nachmittag", "Gute Nacht", "Auf Wiedersehen"),
+        FR("Salut", "Bonjuor", "Bon après-midi", "Bonne nuit", "Au revoir"),
+        NE("Hoi", "Goedemorgen", "Goedemiddag", "Goedenacht", "Vaarwel"),
+        RU("Privet", "Dobroye utro", "Dobryy den'", "spokoynoy nochi", "Do svidaniya");
+
+        private String hello;
+        private String morning;
+        private String afternoon;
+        private String night;
+        private String bye;
+
+        Greetings(String hello, String morning, String afternoon, String night, String bye) {
+            this.hello = hello;
+            this.morning = morning;
+            this.afternoon = afternoon;
+            this.night = night;
+            this.bye = bye;
+        }
+    }
+
     public MessageBean() {
     }
     
-    public String getMsg() {
+    public String getMsg() {  
         return msg;
     }
     
-    public void setMsg(String value) {
-        switch (value){
-            case "":
-            case "pt":
-                msg = "Alô";
-                break;
-            case "en":
-                msg = "Hello";
-                break;
-            case "de":
-                msg = "Hallo";
-                break;
-            case "fr":
-                msg = "Bonjour";
-                break;
+    public void setMsg(String lang) {
+        msg = this.getHelloGreeting(lang) + ", " + this.getTimeGreeting(lang);
+    }
+    
+    public String getByeMsg() {
+        return byeMsg;
+    }
+    
+    public void setByeMsg(String lang) {
+        lang = lang.toUpperCase();
+        byeMsg = Greetings.valueOf(lang).bye;
+    }
+    
+    public String getHelloGreeting(String lang) {
+        lang = lang.toUpperCase();
+        return Greetings.valueOf(lang).hello;
+    }
+
+    public String getTimeGreeting(String lang) {
+        lang = lang.toUpperCase();
+        int hourOfNow = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        if (hourOfNow >= 0 && hourOfNow < 12) {
+            return Greetings.valueOf(lang).morning;
+        } else if (hourOfNow >= 12 && hourOfNow < 18) {
+            return Greetings.valueOf(lang).afternoon;
+        } else {
+            return Greetings.valueOf(lang).night;
         }
     }
 }
--- src/main/webapp/saygoodbye.jsp
+++ src/main/webapp/saygoodbye.jsp
@@ -1,16 +1,10 @@
-<%-- 
-    Document   : sayhello
-    Created on : 08/02/2021, 23:08:15
-    Author     : viter
---%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>sayhello.jsp</title>
+        <title>saygoodbye.jsp</title>
         <link rel="stylesheet" type="text/css" href="app.css">
     </head>
     <body>
@@ -20,8 +14,8 @@
             <img src="imagens/conversas.png" style="width: 600px;" alt="conversas"/>
 
             <% String lang = request.getParameter("lang"); %>
-            <jsp:setProperty name="myBean" property="msg" value="<%=lang%>"/>
-            <h3><jsp:getProperty name="myBean" property="msg"/> <%=request.getParameter("nome")%>!</h3>
+            <jsp:setProperty name="myBean" property="byeMsg" value="<%=lang%>"/>
+            <h3><jsp:getProperty name="myBean" property="byeMsg"/> <%=request.getParameter("nome")%>!</h3>
             <br/><br/><a href="#" onclick="window.history.back();">Voltar</a>
         </div>
     </body>
